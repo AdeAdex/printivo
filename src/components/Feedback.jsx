c5c12ec08ec169abfdfbaea2a406b52df06ebd34
Adeolu Amole <adeoluamole@gmail.com>
2023-04-10 08:09:48 +0100

    Working on the feedback component

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -8,15 +8,24 @@ const Feedback = () => {
         <div style={{color: '#384A62'}} className="each-feed-container d-flex w-100 gap-4 mt-4">
                 <div className="bg-white container-fluid py-3 d-flex gap-4" style={{height: '160px', width: '33.3%'}}>
                   <i className="fab fa-twitter border" style={{color: '#1DA1F2', width: '35px', height: '35px', borderRadius: '50%', padding: '10px', fontSize: '14px'}}></i>
-                  <div>adex</div>
+                  <div className="mt-2">
+                    <div className="mb-2">Adeolu Amole</div>
+                    <div>I am now the unofficial @Printivo ambassador. What these guys just pulled to ensure I got my menu cards today is stunning!</div>
+                  </div>
                 </div>
                 <div className="bg-white container-fluid py-3 d-flex gap-4" style={{height: '160px', width: '33.3%'}}>
                   <i className="fab fa-twitter border" style={{color: '#1DA1F2', width: '35px', height: '35px', borderRadius: '50%', padding: '10px', fontSize: '14px'}}></i>
-                  <div>adex</div>
+                  <div className="mt-2">
+                    <div className="mb-2">Adeolu Amole</div>
+                    <div>I am now the unofficial @Printivo ambassador. What these guys just pulled to ensure I got my menu cards today is stunning!</div>
+                  </div>
                 </div>
                 <div className="bg-white container-fluid py-3 d-flex gap-4" style={{height: '160px', width: '33.3%'}}>
                   <i className="fab fa-twitter border" style={{color: '#1DA1F2', width: '35px', height: '35px', borderRadius: '50%', padding: '10px', fontSize: '14px'}}></i>
-                  <div>adex</div>
+                  <div className="mt-2">
+                    <div className="mb-2">Adeolu Amole</div>
+                    <div>I am now the unofficial @Printivo ambassador. What these guys just pulled to ensure I got my menu cards today is stunning!</div>
+                  </div>
                 </div>
         </div>
       </section>
